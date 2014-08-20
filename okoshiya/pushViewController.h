27c17807b5a81f4965bda8622f65bd9dd70b4af8
芹生 義雄 <seryu623@serisei-no-MacBook-Air.local>
2014-08-20 11:03:47 +0900

    あああ

---
+++ okoshiya/pushViewController.h
@@ -0,0 +1,13 @@
+//
+//  pushViewController.h
+//  okoshiya
+//
+//  Created by 芹生 義雄 on 2014/08/20.
+//  Copyright (c) 2014年 hata kanae. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface pushViewController : UIViewController
+
+@end
